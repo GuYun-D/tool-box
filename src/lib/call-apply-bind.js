4d1c49fbb9a99572bed719568ff833750afd82ff
GuYun-D <1946306647@qq.com>
2022-03-22 21:09:06 +0800

    修复bind可以多次调用的bug

--- src/lib/call-apply-bind.js
+++ src/lib/call-apply-bind.js
@@ -17,14 +17,17 @@ Function.prototype.myApply = function (thisArgs, ...args) {
 }
 
 Function.prototype.myBind = function (thisArgs, ...args1) {
+  let index = 0
   const fn = this
   thisArgs = (thisArgs !== null && thisArgs !== undefined) ? Object(thisArgs) : window
 
   return function (...args2) {
+    if (index === 1) return;
     thisArgs.fn = fn
     const fianlParam = [...args1, ...args2]
     const result = thisArgs.fn(...fianlParam)
     delete thisArgs.fn
+    index++
     return result
   }
 }
